3b5399f4c07dd93446ee73e914b4d8e155eb5907
Wirunpong Jaingamlertwong <it58070126@office365.it.kmitl.ac.th>
2017-08-18 11:30:38 +0700

    Workshop5
    send lab02 workshop5

--- lab02/HelloLab02/app/src/main/java/lab/bellkung/hellolab02/MainActivity.java
+++ lab02/HelloLab02/app/src/main/java/lab/bellkung/hellolab02/MainActivity.java
@@ -8,6 +8,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.workshop3);
+        setContentView(R.layout.workshop5);
     }
 }
