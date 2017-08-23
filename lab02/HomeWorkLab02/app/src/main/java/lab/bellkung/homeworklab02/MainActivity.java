d446e3ec6664becf7ac373b2d6fc8f97cefd3959
Wirunpong Jaingamlertwong <it58070126@office365.it.kmitl.ac.th>
2017-08-23 22:08:34 +0700

    HomeWorkLab02-2
    send homework2

--- lab02/HomeWorkLab02/app/src/main/java/lab/bellkung/homeworklab02/MainActivity.java
+++ lab02/HomeWorkLab02/app/src/main/java/lab/bellkung/homeworklab02/MainActivity.java
@@ -8,6 +8,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.homework_01);
+        setContentView(R.layout.homework_02);
     }
 }
