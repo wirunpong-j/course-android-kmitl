b2e38ec8adcf4bb7bfdbe45def4e77f0a889897f
Wirunpong Jaingamlertwong <it58070126@office365.it.kmitl.ac.th>
2017-08-25 11:22:09 +0700

    create view and model
    SimpleMyDot Project

--- lab/lab03/SimpleMyDot/app/src/main/java/kmitl/lab03/wirunpong58070126/MainActivity.java
+++ lab/lab03/SimpleMyDot/app/src/main/java/kmitl/lab03/wirunpong58070126/MainActivity.java
@@ -2,6 +2,10 @@ package kmitl.lab03.wirunpong58070126;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+
+    public void onRandomDot(View view) {
+        Random rd = new Random();
+        int centerX = rd.nextInt(500);
+        int centerY = rd.nextInt(500);
+
+        TextView x = (TextView) findViewById(R.id.randomNum);
+        x.setText("X : " + centerX + ", Y : " + centerY);
+
     }
 }
---
+++ lab/lab03/SimpleMyDot/app/src/main/java/kmitl/lab03/wirunpong58070126/model/Dot.java
@@ -0,0 +1,14 @@
+package kmitl.lab03.wirunpong58070126.model;
+
+/**
+ * Created by BellKunG on 8/25/2017 AD.
+ */
+
+public class Dot {
+    private int centerX;
+    private int centerY;
+    private int radius;
+
+    
+
+}
