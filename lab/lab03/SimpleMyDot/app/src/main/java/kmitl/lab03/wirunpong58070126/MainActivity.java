4ce9cf7313a84e10c95cf45fd81b0d8de1780c25
Wirunpong Jaingamlertwong <it58070126@office365.it.kmitl.ac.th>
2017-08-25 13:02:27 +0700

    add dot on display
    SimpleMyDot

--- lab/lab03/SimpleMyDot/app/src/main/java/kmitl/lab03/wirunpong58070126/MainActivity.java
+++ lab/lab03/SimpleMyDot/app/src/main/java/kmitl/lab03/wirunpong58070126/MainActivity.java
@@ -7,22 +7,34 @@ import android.widget.TextView;
 
 import java.util.Random;
 
-public class MainActivity extends AppCompatActivity {
+import kmitl.lab03.wirunpong58070126.model.Dot;
+import kmitl.lab03.wirunpong58070126.view.DotView;
+
+public class MainActivity extends AppCompatActivity implements Dot.DotChangedListener {
+
+    private Dot dot;
+    private DotView dotView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
+        dot = new Dot(0, 0, 50);
+        dot.setDotChangedListener(this);
+
+        dotView = (DotView) findViewById(R.id.dotView);
+    }
 
     public void onRandomDot(View view) {
         Random rd = new Random();
-        int centerX = rd.nextInt(500);
-        int centerY = rd.nextInt(500);
+        dot.setCenterX(rd.nextInt(dotView.getWidth()));
+        dot.setCenterY(rd.nextInt(dotView.getHeight()));
+    }
 
-        TextView x = (TextView) findViewById(R.id.randomNum);
-        x.setText("X : " + centerX + ", Y : " + centerY);
-
+    @Override
+    public void onDotChanged(Dot dot) {
+        dotView.setDot(dot);
+        dotView.invalidate();
     }
 }
--- lab/lab03/SimpleMyDot/app/src/main/java/kmitl/lab03/wirunpong58070126/model/Dot.java
+++ lab/lab03/SimpleMyDot/app/src/main/java/kmitl/lab03/wirunpong58070126/model/Dot.java
@@ -1,14 +1,58 @@
 package kmitl.lab03.wirunpong58070126.model;
 
-/**
- * Created by BellKunG on 8/25/2017 AD.
- */
-
 public class Dot {
     private int centerX;
     private int centerY;
     private int radius;
+    private DotChangedListener dotChangedListener;
 
-    
+    public interface DotChangedListener {
+        void onDotChanged(Dot dot);
+    }
 
+    public void setDotChangedListener(DotChangedListener dotChangedListener) {
+        this.dotChangedListener = dotChangedListener;
+    }
+
+    public Dot(DotChangedListener dotChangedListener, int centerX, int centerY, int radius) {
+        this.dotChangedListener = dotChangedListener;
+        this.centerX = centerX;
+        this.centerY = centerY;
+        this.radius = radius;
+
+        this.dotChangedListener.onDotChanged(this);
+
+    }
+
+    public Dot(int centerX, int centerY, int radius) {
+        this.centerX = centerX;
+        this.centerY = centerY;
+        this.radius = radius;
+    }
+
+    public int getCenterX() {
+        return centerX;
+    }
+
+    public void setCenterX(int centerX) {
+        this.centerX = centerX;
+        this.dotChangedListener.onDotChanged(this);
+    }
+
+    public int getCenterY() {
+        return centerY;
+    }
+
+    public void setCenterY(int centerY) {
+        this.centerY = centerY;
+        this.dotChangedListener.onDotChanged(this);
+    }
+
+    public int getRadius() {
+        return radius;
+    }
+
+    public void setRadius(int radius) {
+        this.radius = radius;
+    }
 }
