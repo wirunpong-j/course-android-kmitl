76e0621369872459389ea9c3affd442c652e189d
Wirunpong Jaingamlertwong <it58070126@office365.it.kmitl.ac.th>
2017-10-13 22:54:45 +0700

    rename project

---
+++ lab/lab07/MyLazyInstagram/app/src/main/java/lab07/bellkung/mylazyinstagram/api/LazyInstagramAPI.java
@@ -0,0 +1,23 @@
+package lab07.bellkung.mylazyinstagram.api;
+
+import lab07.bellkung.mylazyinstagram.model.UserProfile;
+import retrofit2.Call;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.GET;
+import retrofit2.http.POST;
+import retrofit2.http.Query;
+
+/**
+ * Created by BellKunG on 13/10/2017 AD.
+ */
+
+public interface LazyInstagramAPI {
+    String BASE = "https://us-central1-retrofit-course.cloudfunctions.net";
+    @GET("/getProfile")
+    Call<UserProfile> getProfile(@Query("user") String user);
+
+    @FormUrlEncoded
+    @POST("/follow")
+    Call<UserProfile> postProfile(@Field("user") String user, @Field("isFollow") boolean isFollow);
+}
---
+++ lab/lab07/MyLazyInstagram/app/src/main/java/lab07/bellkung/mylazyinstagram/model/PostsModel.java
@@ -0,0 +1,37 @@
+package lab07.bellkung.mylazyinstagram.model;
+
+/**
+ * Created by BellKunG on 13/10/2017 AD.
+ */
+
+public class PostsModel {
+    private int comment;
+    private int like;
+    private String url;
+
+    public int getComment() {
+        return comment;
+    }
+
+    public void setComment(int comment) {
+        this.comment = comment;
+    }
+
+    public int getLike() {
+        return like;
+    }
+
+    public void setLike(int like) {
+        this.like = like;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+}
+
